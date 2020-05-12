90eee95f1c7009fd2877f524352e1d8f56669719
Netrve <35631977+Netrve@users.noreply.github.com>
2020-05-12 19:16:50 +0200

    Add files via upload

--- Source/AI/WorkGiver_HaulUrgently.cs
+++ Source/AI/WorkGiver_HaulUrgently.cs
@@ -1,35 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AllowTool.Comps;
+using JetBrains.Annotations;
 using RimWorld;
 using Verse;
 using Verse.AI;
 
-namespace AllowTool {
-	// Generates hauling jobs for things designated for urgent hauling
-	public class WorkGiver_HaulUrgently : WorkGiver_Scanner {
-		public delegate Job TryGetJobOnThing(Pawn pawn, Thing t, bool forced);
-		
-		// give a vanilla haul job- it works just fine for our needs
-		public static TryGetJobOnThing JobOnThingDelegate = (pawn, t, forced) => HaulAIUtility.HaulToStorageJob(pawn, t);
+namespace AllowTool
+{
+    // Generates hauling jobs for things designated for urgent hauling
+    [UsedImplicitly]
+    public class WorkGiver_HaulUrgently : WorkGiver_Scanner
+    {
+        public delegate Job TryGetJobOnThing(Pawn pawn, Thing t, bool forced);
 
-		public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false) {
-			return JobOnThingDelegate(pawn, t, forced);
-		}
+        // give a vanilla haul job- it works just fine for our needs
+        public static TryGetJobOnThing JobOnThingDelegate = (pawn, t, forced) => HaulAIUtility.HaulToStorageJob(pawn, t);
 
-		public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn) {
-			var designations = pawn.Map.designationManager.allDesignations;
-			// look over all designations
-			for (int i = 0; i < designations.Count; i++) {
-				var des = designations[i];
-				// find our haul designation
-				if (des.def != AllowToolDefOf.HaulUrgentlyDesignation) continue;
-				var thing = des.target.Thing;
-				// make sure the designated thing is a valid candidate for hauling
-				if (thing?.def != null && (thing.def.alwaysHaulable || thing.def.EverHaulable) 
-					&& !thing.IsInValidBestStorage() && HaulAIUtility.PawnCanAutomaticallyHaulFast(pawn, thing, false)) {
-					yield return thing;
-				}
-			}
-		}
-	}
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            return JobOnThingDelegate(pawn, t, forced);
+        }
+
+        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+        {
+            // look over all saved things
+            return pawn.Map.GetComponent<MapComp_AllowTool>().designatedThings.Where(thing =>
+                thing?.def != null && (thing.def.alwaysHaulable || thing.def.EverHaulable) && !thing.IsInValidBestStorage() &&
+                HaulAIUtility.PawnCanAutomaticallyHaulFast(pawn, thing, false));
+        }
+    }
 }
