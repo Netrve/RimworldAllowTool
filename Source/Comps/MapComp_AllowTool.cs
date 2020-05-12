2fb39a2334a58cc6ee236e25e83dbe3762435d44
Netrve <35631977+Netrve@users.noreply.github.com>
2020-05-12 19:16:26 +0200

    Add files via upload

---
+++ Source/Comps/MapComp_AllowTool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Verse;
+
+namespace AllowTool.Comps
+{
+    [UsedImplicitly]
+    public class MapComp_AllowTool : MapComponent
+    {
+        public readonly List<Thing> designatedThings = new List<Thing>();
+
+        public MapComp_AllowTool(Map map) : base(map) 
+        {
+        }
+    }
+}
