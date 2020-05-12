329b62b49a90ef00280395736cd7725c4b60b3b3
Netrve <35631977+Netrve@users.noreply.github.com>
2020-05-12 19:17:49 +0200

    Add files via upload

---
+++ Source/Patches/DesignationManager_Patch.cs
@@ -0,0 +1,30 @@
+﻿using AllowTool.Comps;
+using HarmonyLib;
+using Verse;
+
+namespace AllowTool.Patches
+{
+    [HarmonyPatch(typeof(DesignationManager), nameof(DesignationManager.RemoveAllDesignationsOn))]
+    internal static class DesignationManagerRAD_Patch
+    {
+        [HarmonyPrefix]
+        public static void RemoveAllDesignationsOn(DesignationManager __instance, Thing t)
+        {
+            if (__instance.map.designationManager.DesignationOn(t, AllowToolDefOf.HaulUrgentlyDesignation) == null) return;
+            
+            __instance.map.GetComponent<MapComp_AllowTool>().designatedThings.Remove(t);
+        }
+    }
+
+    [HarmonyPatch(typeof(DesignationManager), nameof(DesignationManager.RemoveDesignation))]
+    internal static class DesignationManagerRD_Patch
+    {
+        [HarmonyPostfix]
+        public static void RemoveDesignation(DesignationManager __instance, Designation des)
+        {
+            if (des.def != AllowToolDefOf.HaulUrgentlyDesignation) return;
+            
+            __instance.map.GetComponent<MapComp_AllowTool>().designatedThings.Remove(des.target.Thing);
+        }
+    }
+}
